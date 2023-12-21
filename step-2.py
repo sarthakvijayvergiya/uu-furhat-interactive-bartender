a8b5e81cbd9cdbb56e52bac2810b2317a81d9ea4
Sarthak Vijayvergiya <18403281+sarthakvijayvergiya@users.noreply.github.com>
2023-12-21 17:30:42 +0100

    Model training and testing code

--- step-2.py
+++ step-2.py
@@ -1,6 +1,103 @@
+# import warnings
+# import pandas as pd
+# from sklearn.model_selection import train_test_split, GridSearchCV
+# from sklearn.tree import DecisionTreeClassifier
+# from sklearn.neighbors import KNeighborsClassifier
+# from sklearn.metrics import accuracy_score
+# from sklearn.preprocessing import StandardScaler
+# from sklearn.impute import SimpleImputer
+# from sklearn.pipeline import Pipeline
+# import joblib
+# from sklearn.svm import SVC
+
+# # Suppress future warnings
+# warnings.simplefilter(action='ignore', category=FutureWarning)
+
+# def load_and_display_data():
+#     data = pd.read_csv("processed/aus.csv")  # Load your dataset
+#     class_distribution = data["expression"].value_counts()
+#     print("Class distribution:")
+#     print(class_distribution)
+#     return data
+
+# def split_data(data):
+#     labels = data["expression"]
+#     features = data.drop(["file", "face", "expression", "valence", "arousal"], axis=1)  # Exclude non-feature columns
+
+#     X_train, X_test, y_train, y_test = train_test_split(
+#         features, labels, test_size=0.2, random_state=42, stratify=labels)
+
+#     return X_train, X_test, y_train, y_test
+
+# def scale_data(X_train, X_test):
+#     scaler = StandardScaler()
+#     X_train_scaled = scaler.fit_transform(X_train)
+#     X_test_scaled = scaler.transform(X_test)
+#     return X_train_scaled, X_test_scaled
+
+# def train_and_evaluate(model, X_train, y_train, X_test, y_test):
+#     # Create a pipeline that first imputes missing values, then fits the model
+#     pipeline = Pipeline([
+#         ('imputer', SimpleImputer(strategy='median')),  # You can change strategy to 'mean' or 'most_frequent'
+#         ('model', model)
+#     ])
+    
+#     pipeline.fit(X_train, y_train)
+#     test_predictions = pipeline.predict(X_test)
+#     test_accuracy = accuracy_score(y_test, test_predictions)
+#     print(f"Test Accuracy for {type(model).__name__}: {test_accuracy:.2f}")
+#     return test_accuracy
+
+# def tune_hyperparameters(X_train, y_train):
+#     """ Perform hyperparameter tuning for an SVM model """
+#     param_grid = [
+#         {"kernel": ["poly"], "degree": [3, 15, 25, 50]},
+#         {"kernel": ["rbf", "linear", "sigmoid"]}
+#     ]
+#     grid_search = GridSearchCV(SVC(), param_grid, cv=5)
+#     grid_search.fit(X_train, y_train)
+#     print("Optimal parameters for SVC:", grid_search.best_params_)
+#     return grid_search
+
+# def main():
+#     data = load_and_display_data()
+#     X_train, X_test, y_train, y_test = split_data(data)
+#     X_train_scaled, X_test_scaled = scale_data(X_train, X_test)
+
+#     models = {
+#         "DecisionTreeClassifier": DecisionTreeClassifier(random_state=42),
+#         "KNeighborsClassifier": KNeighborsClassifier()
+#     }
+
+#     best_accuracy = 0
+#     best_model_name = ""
+#     best_model = None
+
+#     for name, model in models.items():
+#         print(f"Training and evaluating {name}")
+#         accuracy = train_and_evaluate(model, X_train_scaled, y_train, X_test_scaled, y_test)
+#         if accuracy > best_accuracy:
+#             best_accuracy = accuracy
+#             best_model = model
+#             best_model_name = name
+    
+#     print(f"The best model is {best_model_name} with an accuracy of {best_accuracy:.2f}")
+
+#     # Save the best performing model
+#     if best_model is not None:
+#         joblib.dump(best_model, 'best_emotion_recognition_model.pkl')
+#         print(f"Saved the best model ({best_model_name}) to 'best_emotion_recognition_model.pkl'.")
+
+#     svm_model = tune_hyperparameters(X_train_scaled, y_train)
+#     accuracy = train_and_evaluate(svm_model, X_train_scaled, y_train)
+#     print(f"Test Set Accuracy for SVM: {accuracy:.2f}")
+# if __name__ == "__main__":
+#     main()
+
+
 import warnings
 import pandas as pd
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split, GridSearchCV
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.metrics import accuracy_score
@@ -8,6 +105,9 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.impute import SimpleImputer
 from sklearn.pipeline import Pipeline
 import joblib
+from sklearn.svm import SVC
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import RandomizedSearchCV
 
 # Suppress future warnings
 warnings.simplefilter(action='ignore', category=FutureWarning)
@@ -21,36 +121,81 @@ def load_and_display_data():
 
 def split_data(data):
     labels = data["expression"]
-    features = data.drop(["file", "face", "expression"], axis=1)  # Exclude non-feature columns
+    features = data.drop(["file", "face", "expression", "valence", "arousal"], axis=1)
 
-    X_train, X_test, y_train, y_test = train_test_split(
+    # Split into training and test set
+    X_temp, X_test, y_temp, y_test = train_test_split(
         features, labels, test_size=0.2, random_state=42, stratify=labels)
 
-    return X_train, X_test, y_train, y_test
+    # Further split the training set into training and validation set
+    X_train, X_val, y_train, y_val = train_test_split(
+        X_temp, y_temp, test_size=0.25, random_state=42)  # 0.25 * 0.8 = 0.2
 
-def scale_data(X_train, X_test):
+    return X_train, X_val, X_test, y_train, y_val, y_test
+
+def scale_data(X_train, X_val, X_test):
     scaler = StandardScaler()
     X_train_scaled = scaler.fit_transform(X_train)
+    X_val_scaled = scaler.transform(X_val)
     X_test_scaled = scaler.transform(X_test)
-    return X_train_scaled, X_test_scaled
+    return X_train_scaled, X_val_scaled, X_test_scaled
 
-def train_and_evaluate(model, X_train, y_train, X_test, y_test):
-    # Create a pipeline that first imputes missing values, then fits the model
+def train_and_evaluate(model, X_train, y_train, X_val, y_val):
     pipeline = Pipeline([
-        ('imputer', SimpleImputer(strategy='median')),  # You can change strategy to 'mean' or 'most_frequent'
+        ('imputer', SimpleImputer(strategy='median')),
         ('model', model)
     ])
-    
     pipeline.fit(X_train, y_train)
-    test_predictions = pipeline.predict(X_test)
-    test_accuracy = accuracy_score(y_test, test_predictions)
-    print(f"Test Accuracy for {type(model).__name__}: {test_accuracy:.2f}")
-    return test_accuracy
+    val_predictions = pipeline.predict(X_val)
+    val_accuracy = accuracy_score(y_val, val_predictions)
+    print(f"Validation Accuracy for {type(model).__name__}: {val_accuracy:.2f}")
+    return val_accuracy
+
+def tune_hyperparameters(X_train, y_train):
+    """ Perform hyperparameter tuning for an SVM model """
+    param_grid = [
+        {"model__kernel": ["poly"], "model__degree": [3, 15, 25, 50]},
+        {"model__kernel": ["rbf", "linear", "sigmoid"]}
+    ]
+    pipeline = Pipeline([
+        ('imputer', SimpleImputer(strategy='median')),
+        ('scaler', StandardScaler()),
+        ('model', SVC())
+    ])
+    grid_search = GridSearchCV(pipeline, param_grid, cv=5)
+    grid_search.fit(X_train, y_train)
+    print("Optimal parameters for SVC:", grid_search.best_params_)
+    return grid_search
+
+def tune_random_forest(X_train, y_train):
+    # Define parameter grid
+    param_grid = {
+        'model__n_estimators': [100, 200, 300],
+        'model__max_depth': [10, 20, 30, None],
+        'model__min_samples_split': [2, 5, 10],
+        # Add other parameters to test
+    }
+
+    # Create a pipeline with imputer, scaler, and random forest
+    pipeline = Pipeline([
+        ('imputer', SimpleImputer(strategy='median')),
+        ('scaler', StandardScaler()),  # Optional: include if feature scaling is beneficial
+        ('model', RandomForestClassifier(random_state=42))
+    ])
+
+    # Create randomized search object
+    randomized_search = RandomizedSearchCV(pipeline, param_grid, n_iter=100, cv=5, random_state=42)
+    randomized_search.fit(X_train, y_train)
+
+    # Best parameters and model
+    print("Best Parameters:", randomized_search.best_params_)
+    return randomized_search
+
 
 def main():
     data = load_and_display_data()
-    X_train, X_test, y_train, y_test = split_data(data)
-    X_train_scaled, X_test_scaled = scale_data(X_train, X_test)
+    X_train, X_val, X_test, y_train, y_val, y_test = split_data(data)
+    X_train_scaled, X_val_scaled, X_test_scaled = scale_data(X_train, X_val, X_test)
 
     models = {
         "DecisionTreeClassifier": DecisionTreeClassifier(random_state=42),
@@ -63,19 +208,27 @@ def main():
 
     for name, model in models.items():
         print(f"Training and evaluating {name}")
-        accuracy = train_and_evaluate(model, X_train_scaled, y_train, X_test_scaled, y_test)
+        accuracy = train_and_evaluate(model, X_train_scaled, y_train, X_val_scaled, y_val)
         if accuracy > best_accuracy:
             best_accuracy = accuracy
             best_model = model
             best_model_name = name
 
-    print(f"The best model is {best_model_name} with an accuracy of {best_accuracy:.2f}")
+    print(f"The best non-SVM model is {best_model_name} with a validation accuracy of {best_accuracy:.2f}")
 
-    # Save the best performing model
+    # Save the best performing non-SVM model
     if best_model is not None:
-        joblib.dump(best_model, 'best_emotion_recognition_model.pkl')
-        print(f"Saved the best model ({best_model_name}) to 'best_emotion_recognition_model.pkl'.")
+        joblib.dump(best_model, 'best_emotion_recognition_model_non_svm.pkl')
+        print(f"Saved the best non-SVM model ({best_model_name}) to 'best_emotion_recognition_model_non_svm.pkl'.")
+
+    # SVM model tuning and evaluation
+    svm_model = tune_hyperparameters(X_train_scaled, y_train).best_estimator_
+    svm_accuracy = train_and_evaluate(svm_model, X_train_scaled, y_train, X_val_scaled, y_val)
+    print(f"Validation Set Accuracy for SVM: {svm_accuracy:.2f}")
+
+    # Save the tuned SVM model
+    joblib.dump(svm_model, 'best_emotion_recognition_model_svm.pkl')
+    print("Saved the tuned SVM model to 'best_emotion_recognition_model_svm.pkl'.")
 
 if __name__ == "__main__":
     main()
-
